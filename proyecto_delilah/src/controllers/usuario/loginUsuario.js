b4b70c8ad5b9ff5a7f7ae5f33f7d31768ced1f70
Mauri Cucco <mscucco@hotmail.com>
2020-10-24 17:47:05 -0300

    Agruegue un try catch al controller de login

--- proyecto_delilah/src/controllers/usuario/loginUsuario.js
+++ proyecto_delilah/src/controllers/usuario/loginUsuario.js
@@ -1,12 +1,17 @@
 const crearToken = require("../../models/crearToken");
 
-async function loginUsuario(req, res) {
-
-    const {id_usuario, nombre_completo, direccion, administrador} = req.infoUsuario;
-
-    const token = crearToken({id_usuario, administrador});
-
-    res.status(200).send({mensaje: "Autenticación exitosa", token, nombre_completo, direccion});
+const loginUsuario = (req, res) => {
+    try {
+        const {id_usuario, nombre_completo, direccion, administrador} = req.infoUsuario;
+    
+        const token = crearToken({id_usuario, administrador});
+    
+        res.status(200).send({mensaje: "Autenticación exitosa", token, nombre_completo, direccion});
+        
+    } catch (error) {
+        
+        res.status(401).send(error);
+    }
 }
 
 module.exports = loginUsuario;
