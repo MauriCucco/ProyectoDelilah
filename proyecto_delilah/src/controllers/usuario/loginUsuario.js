2409883a469a35650c5270796a93ecb7dc51caa7
Mauri Cucco <mscucco@hotmail.com>
2020-10-30 23:13:51 -0300

    Saque los assets

--- proyecto_delilah/src/controllers/usuario/loginUsuario.js
+++ proyecto_delilah/src/controllers/usuario/loginUsuario.js
@@ -11,7 +11,7 @@ const loginUsuario = (req, res) => {
         
     } catch (error) {
         
-        res.status(401).send({error});
+        res.status(500).send({error});
     }
 }
 
--- proyecto_delilah/src/middlewares/admin/verificarAdmin.js
+++ proyecto_delilah/src/middlewares/admin/verificarAdmin.js
@@ -1,14 +1,6 @@
 
-const verificarAdmin = (req, res, next) => {
+const verificarAdmin = (req, res, next) => 
 
-    if(req.user.administrador === "T") {
-
-        next();
-
-    }else {
-
-        res.status(401).send({error: "No posee autorización de administrador"});
-    }
-}
-
+    (req.user.administrador === "T")? next() : res.status(401).send({error: "No posee autorización de administrador"});
+    
 module.exports = verificarAdmin;
